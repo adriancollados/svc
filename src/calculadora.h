8d755faec1641ee072817eb0d969e817a6005ca0
Adrian <adricm32@gmail.com>
2021-12-29 18:41:36 +0100

    todo ok

--- src/calculadora.h
+++ src/calculadora.h
@@ -1,12 +1,14 @@
 #ifndef OP
 #define OP
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 double sumar(double, double);
 double restar(double, double);
 double multiplicar(double, double);
 double dividir(double, double);
+double raiz(double);
 
 #include "operacion.cc"
 #endif
